b6e0ac67ce993d89d05e040cd8ce651bcef86f9a
Linda Levin <linda@onlinecoach.nu>
2020-12-29 11:59:58 +0100

    Initial Commit

---
+++ EmoMemoApp/EmoMemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmoMemoAppApp.swift
+//  EmoMemoApp
+//
+//  Created by Linda Levin on 2020-12-29.
+//
+
+import SwiftUI
+
+@main
+struct EmoMemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
